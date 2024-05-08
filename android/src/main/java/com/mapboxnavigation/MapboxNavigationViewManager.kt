3ad6ca8e52685b0eddd317c34093bd979e432722
Fabio Greter <fabio.greter@jls.ch>
2024-05-08 13:11:11 +0200

    Add Mapbox dependencies

--- android/src/main/java/com/mapboxnavigation/MapboxNavigationViewManager.kt
+++ android/src/main/java/com/mapboxnavigation/MapboxNavigationViewManager.kt
@@ -1,13 +1,33 @@
 package com.mapboxnavigation
 
+import android.content.pm.PackageManager
 import android.util.Log
 import android.view.View
+import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 
-class MapboxNavigationViewManager : SimpleViewManager<View>() {
+class MapboxNavigationViewManager(private var mCallerContext: ReactApplicationContext) : SimpleViewManager<View>() {
+  private var accessToken: String? = null
+
+  init {
+    mCallerContext.runOnUiQueueThread {
+      try {
+        val app = mCallerContext.packageManager.getApplicationInfo(mCallerContext.packageName, PackageManager.GET_META_DATA)
+        val bundle = app.metaData
+        val accessToken = bundle.getString("MAPBOX_ACCESS_TOKEN")
+        this.accessToken = accessToken
+//        ResourceOptionsManager.getDefault(mCallerContext, accessToken).update {
+//          tileStoreUsageMode(TileStoreUsageMode.READ_ONLY)
+//        }
+      } catch (e: PackageManager.NameNotFoundException) {
+        e.printStackTrace()
+      }
+    }
+  }
+
   override fun getName() = "MapboxNavigationView"
 
   override fun createViewInstance(reactContext: ThemedReactContext): View {
