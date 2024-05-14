6793f556eeee40f24d40013fd21e80d61452ab58
Fabio Greter <fabio.greter@jls.ch>
2024-05-14 14:58:35 +0200

    Android: Add location puck

--- android/src/main/java/ch/jls/reactnative/mapboxnavigation/mapbox/CustomMapboxNavigationObserver.kt
+++ android/src/main/java/ch/jls/reactnative/mapboxnavigation/mapbox/CustomMapboxNavigationObserver.kt
@@ -2,9 +2,10 @@ package ch.jls.reactnative.mapboxnavigation.mapbox
 
 import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
+import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
 
-class CustomMapboxNavigationObserver : MapboxNavigationObserver {
-  private val locationObserver = CustomLocationObserver()
+class CustomMapboxNavigationObserver(navigationLocationProvider: NavigationLocationProvider) : MapboxNavigationObserver {
+  private val locationObserver = CustomLocationObserver(navigationLocationProvider)
 
   override fun onAttached(mapboxNavigation: MapboxNavigation) {
     mapboxNavigation.registerLocationObserver(locationObserver)
